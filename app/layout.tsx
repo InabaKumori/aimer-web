f1373c10dc9a8ba13d0c5c60ce8ca402e660cf85
WereIYou <userloggedout@gmail.com>
2023-07-03 11:49:58 +0800

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "Aimer-web.com",
+		template: "%s | Aimer-web.com",
 	},
 	description: "Software engineer at upstash.com and founder of planetfall.io",
 	openGraph: {
-		title: "chronark.com",
+		title: "Aimer-web.com",
 		description:
 			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+		url: "https://Aimer-web.com",
+		siteName: "Aimer-web.com",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "https://Aimer-web.com/og.png",
 				width: 1920,
 				height: 1080,
 			},
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 		},
 	},
 	twitter: {
-		title: "Chronark",
+		title: "Aimer-web",
 		card: "summary_large_image",
 	},
 	icons: {
