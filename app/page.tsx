e0d6a4156ce5472bf0e37c17af9e7ac42b7f1419
root <userloggedout@gmail.com>
2023-07-04 15:36:18 -0400

    Added client site component while improving mobile viewing experiences

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 import Link from "next/link";
-import React from "react";
+import React from 'react';
 import Particles from "./components/particles";
+import { useState, useEffect } from 'react';
+
 
 const navigation = [
 	{ name: "Projects", href: "/projects" },
@@ -9,6 +12,28 @@ const navigation = [
 
 
 export default function Home() {
+
+	const [isPortrait, setIsPortrait] = useState(false);
+
+	useEffect(() => {
+		const checkOrientation = () => {
+	  		if (window.innerWidth < window.innerHeight) {
+				setIsPortrait(true);
+	  		} else {
+				setIsPortrait(false);
+	  		}
+		};
+	
+		// Initial check
+		checkOrientation();
+  
+		// Listen for window resize events
+		window.addEventListener('resize', checkOrientation);
+  
+		// Cleanup
+		return () => window.removeEventListener('resize', checkOrientation);
+  	}, []);
+
 	return (
 		<div className="fixed flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 
@@ -23,19 +48,19 @@ export default function Home() {
 				 }}
 			/>
 
-			<div className="sm:hidden fixed inset-0 backdrop-blur-md bg-white bg-opacity-20" />
+	{isPortrait && <div className="fixed inset-0 backdrop-blur-md bg-white bg-opacity-20" />}
 		
 		
 			<div style={{    position: 'fixed',
-    top: 0, // start from top
-    left: 0, // start from left
-    width: '100%', // full width
-    height: '100%', // full height
-    display: 'flex',
-    alignItems: 'center', // center vertically in the container
-    justifyContent: 'center', // center horizontally in the container
-	flexDirection: 'column', // stack children vertically
-    zIndex: 40,}}>
+			top: 0, // start from top
+			left: 0, // start from left
+			width: '100%', // full width
+			height: '100%', // full height
+			display: 'flex',
+			alignItems: 'center', // center vertically in the container
+			justifyContent: 'center', // center horizontally in the container
+			flexDirection: 'column', // stack children vertically
+			zIndex: 40,}}>
 
 			<nav className="my-0 sm:-my-3  animate-fade-in" style={{zIndex: 20}}>
 				<ul className="flex items-center justify-center gap-4">
