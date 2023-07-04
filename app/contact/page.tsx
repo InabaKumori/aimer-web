d47f662dbd06d63cc01bc52fe3beff1a15f9186f
root <userloggedout@gmail.com>
2023-07-03 21:55:10 -0400

    Contact information updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/WereIYou",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "WereIYou",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:info@aimer-web.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "info@aimer-web.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/InabaKumori",
 		label: "Github",
-		handle: "chronark",
+		handle: "InabaKumori",
 	},
 ];
 
