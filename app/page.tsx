83b0fa91a2fb60aa5e68501ff90a7629d4a5966a
root <userloggedout@gmail.com>
2023-07-05 22:16:16 -0400

    Changed the Aimer title font successfully

--- app/page.tsx
+++ app/page.tsx
@@ -46,9 +46,9 @@ export default function Home() {
 
 	return (
 		<>
-		<Head>
-		<link rel="stylesheet" href="https://use.typekit.net/zjl6ven.css" />
-	  </Head>
+            <style>
+                @import url('https://use.typekit.net/zjl6ven.css');
+            </style>
 
 	  
 		<div className="fixed flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
